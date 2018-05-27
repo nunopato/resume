51d32347396a5d8f9e4e6d64a0752deb356cebd8
Nuno Pinge <nuno@pinge.org>
2018-05-28 00:02:25 +0200

    added Dripcil to resume, tweaked UI sizing details

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -59,7 +59,7 @@ const Header = styled.div`
 const SubHeader = styled.div`
   display: flex;
   flex-direction: row;
-  margin-bottom: 3mm;
+  margin-bottom: 2mm;
   justify-content: space-between;
 `
 
@@ -91,7 +91,7 @@ const Title = styled.div`
 const Period = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 6mm;
+  margin-bottom: 5mm;
 `
 
 const Company = styled.div`
@@ -167,7 +167,7 @@ export default class Resume extends Component {
             <Company>
               <Logo style={ { width: '40mm' } }><use xlinkHref="/assets/svg/deployeth.svg#deployeth"></use></Logo>
               <Location>
-                <svg style={ { height: '7mm', width: '7mm' } }>
+                <svg style={ { height: '6mm', width: '6mm' } }>
                   <use xlinkHref="/assets/svg/location.svg#location"></use>
                 </svg>
                 <LocationName>Barcelona</LocationName>
@@ -186,7 +186,7 @@ export default class Resume extends Component {
             <Company>
               <Logo style={ { width: '44mm' } }><use xlinkHref="/assets/svg/startupbootcamp.svg#startupbootcamp"></use></Logo>
               <Location>
-                <svg style={ { height: '7mm', width: '7mm' } }>
+                <svg style={ { height: '6mm', width: '6mm' } }>
                   <use xlinkHref="/assets/svg/location.svg#location"></use>
                 </svg>
                 <LocationName>Barcelona</LocationName>
@@ -194,7 +194,7 @@ export default class Resume extends Component {
             </Company>
             <Role>Entrepreneur<DateRange>| &nbsp;Nov '16 - May '17</DateRange></Role>
             <Responsibilities>
-              <Responsibility>Business development, marketing, sales, and public speaking training</Responsibility>
+              <Responsibility>Business and product development, marketing, sales, and public speaking training</Responsibility>
             </Responsibilities>
           </Period>
 
@@ -202,7 +202,7 @@ export default class Resume extends Component {
             <Company>
               <Logo style={ { width: '31mm' } }><use xlinkHref="/assets/svg/jwplayer.svg#jwplayer"></use></Logo>
               <Location>
-                <svg style={ { height: '7mm', width: '7mm' } }>
+                <svg style={ { height: '6mm', width: '6mm' } }>
                   <use xlinkHref="/assets/svg/location.svg#location"></use>
                 </svg>
                 <LocationName>New York</LocationName>
@@ -211,16 +211,17 @@ export default class Resume extends Component {
             <Role>Software Engineering Manager, Media Engineering<DateRange>| &nbsp;Aug '16 - Feb '17</DateRange></Role>
             <Responsibilities>
               <Responsibility>Architected and implemented the live streaming product JW Live within a timeline of 6 months</Responsibility>
-              <Responsibility>Built an additional engineering team (Media Intelligence), mentoring interns and promoting internally</Responsibility>
+              <Responsibility>Created the Media Intelligence engineering team, mentoring interns and promoting engineers internally</Responsibility>
+              <Responsibility>Designed and implemented smart thumbnail/scene extraction for video intelligence using machine learning</Responsibility>
               <Responsibility>Designed testing frameworks for automated video ingestion and output quality assessment using SSIM</Responsibility>
               <Responsibility>Speaker at Demuxed 2016: DASHing Into the Wild with VP9</Responsibility>
             </Responsibilities>
             <Role>Software Engineering Manager, Media Services<DateRange>| &nbsp;Aug '15 - Jul '16</DateRange></Role>
             <Responsibilities>
               <Responsibility>Responsible for media management and storage at scale, and content delivery across the globe (PBs per month)</Responsibility>
-              <Responsibility>Built an additional engineering team (Media Platform API), recruiting both engineers and manager</Responsibility>
+              <Responsibility>Created the Media Platform API engineering team, recruiting both engineers and manager</Responsibility>
               <Responsibility>Increased service availability from 98% to 99.98%, by designing a distributed caching layer</Responsibility>
-              <Responsibility>Architected and implemented video adaptive streaming delivery with MPEG-DASH and VP9</Responsibility>
+              <Responsibility>Architected and implemented video adaptive streaming delivery with MPEG-DASH and VP9 video encoding</Responsibility>
               <Responsibility>Speaker at JW Insights: VP9-DASH - Streaming Tomorrow’s Video Today</Responsibility>
               <Responsibility>tech stack: Python, Lua, NGINX, MySQL, PostgreSQL, Redis, AWS</Responsibility>
             </Responsibilities>
@@ -230,7 +231,7 @@ export default class Resume extends Component {
             <Company>
               <Logo style={ { width: '36mm' } }><use xlinkHref="/assets/svg/compstak.svg#compstak"></use></Logo>
               <Location>
-                <svg style={ { height: '7mm', width: '7mm' } }>
+                <svg style={ { height: '6mm', width: '6mm' } }>
                   <use xlinkHref="/assets/svg/location.svg#location"></use>
                 </svg>
                 <LocationName>New York</LocationName>
@@ -238,8 +239,9 @@ export default class Resume extends Component {
             </Company>
             <Role>Senior Software Engineer and CodeSmith<DateRange>| &nbsp;Dec '13 - Jul '15</DateRange></Role>
             <Responsibilities>
-              <Responsibility>API design, frontend and backend architecture and implementation with micro services</Responsibility>
+              <Responsibility>Designed APIs and implemented a micro service oriented architecture backend</Responsibility>
               <Responsibility>Lectured programming classes for the Data Team, mentoring engineers when developing internal tools</Responsibility>
+              <Responsibility>Designed map based user interfaces with features like filtering and aggregating real estate market data</Responsibility>
               <Responsibility>tech stack: JavaScript, Scala, Python, NGINX, MySQL, ElasticSearch, AWS</Responsibility>
             </Responsibilities>
           </Period>
@@ -248,7 +250,7 @@ export default class Resume extends Component {
             <Company>
               <Logo style={ { width: '37mm' } }><use xlinkHref="/assets/svg/livestream.svg#livestream"></use></Logo>
               <Location>
-                <svg style={ { height: '7mm', width: '7mm' } }>
+                <svg style={ { height: '6mm', width: '6mm' } }>
                   <use xlinkHref="/assets/svg/location.svg#location"></use>
                 </svg>
                 <LocationName>New York</LocationName>
@@ -278,7 +280,7 @@ export default class Resume extends Component {
             <Company>
               <Logo style={ { width: '23mm' } }><use xlinkHref="/assets/svg/lmit.svg#lmit"></use></Logo>
               <Location>
-                <svg style={ { height: '7mm', width: '7mm' } }>
+                <svg style={ { height: '6mm', width: '6mm' } }>
                   <use xlinkHref="/assets/svg/location.svg#location"></use>
                 </svg>
                 <LocationName>Lisbon</LocationName>
@@ -286,7 +288,7 @@ export default class Resume extends Component {
             </Company>
             <Role>Head of Software Engineering and Architecture<DateRange>| &nbsp;Jan '12 - Aug '12</DateRange></Role>
             <Responsibilities>
-              <Responsibility>Technical leadership for the WiseMetering Energy Management Platform</Responsibility>
+              <Responsibility>Technical leadership for the WiseMetering energy management platform</Responsibility>
             </Responsibilities>
             <Role>Software Engineer<DateRange>| &nbsp;Dec '08 - Dec '11</DateRange></Role>
             <Responsibilities>
@@ -303,9 +305,26 @@ export default class Resume extends Component {
 
           <Period>
             <Company>
+              <Logo style={ { width: '24mm' } }><use xlinkHref="/assets/svg/dripcil.svg#dripcil"></use></Logo>
+              <Location>
+                <svg style={ { height: '6mm', width: '6mm' } }>
+                  <use xlinkHref="/assets/svg/location.svg#location"></use>
+                </svg>
+                <LocationName>Lisbon</LocationName>
+              </Location>
+            </Company>
+            <Role>Co-Founder and Chief Technology Officer<DateRange>| &nbsp;Aug '11 - present</DateRange></Role>
+            <Responsibilities>
+              <Responsibility>Created DamnSimplePOS, a web based point of sale authorized by the Portuguese Tax Authority</Responsibility>
+              <Responsibility>tech stack: JavaScript, Python, Google Bigtable, Google App Engine</Responsibility>
+            </Responsibilities>
+          </Period>
+
+          <Period>
+            <Company>
               <Logo style={ { width: '38mm' } }><use xlinkHref="/assets/svg/freelancer.svg#freelancer"></use></Logo>
               <Location>
-                <svg style={ { height: '7mm', width: '7mm' } }>
+                <svg style={ { height: '6mm', width: '6mm' } }>
                   <use xlinkHref="/assets/svg/location.svg#location"></use>
                 </svg>
                 <LocationName>Lisbon</LocationName>
@@ -322,7 +341,7 @@ export default class Resume extends Component {
             <Company>
               <Logo style={ { width: '25mm' } }><use xlinkHref="/assets/svg/tecnico.svg#tecnico"></use></Logo>
               <Location>
-                <svg style={ { height: '7mm', width: '7mm' } }>
+                <svg style={ { height: '6mm', width: '6mm' } }>
                   <use xlinkHref="/assets/svg/location.svg#location"></use>
                 </svg>
                 <LocationName>Lisbon</LocationName>
